e51ee9c54ed107981ff199c114780be1d660a548
Dejan Trajkovic <dt@dpab.se>
2025-11-21 02:55:35 +0100

    v

--- src/http/monitorApi.js
+++ src/http/monitorApi.js
@@ -20,7 +20,7 @@ import {
   getSymbolsByCategory
 } from "../market/universe_v2.js";
 import * as OrderbookManager from "../microstructure/OrderbookManager.js";
-import { FeatureEngine } from "../features/featureEngine.js";
+import FeatureEngine from "../features/featureEngine.js";
 
 // PM2 LOG FILE PATHS
 const __filename = fileURLToPath(import.meta.url);
